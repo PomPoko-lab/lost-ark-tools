348bdd7c926a8100fd5b39f194101d471f686eea
ckennx <ckennx@gmail.com>
2022-04-04 12:38:55 -0500

    Sidebar Addition

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const marketPackPrice = document.querySelector(".market--pack-price");
 
 const marketResults = document.querySelector(".market--result");
 
+const appMari = document.getElementById("mari-calc-app");
+const appToDo = document.getElementById("todo-app");
+
+const btnNavMari = document.getElementById("nav--mari");
+const btnNavToDo = document.getElementById("nav--todo");
+
 let mariUnit = 0;
 let mariPrice = 0;
 let mariResult = 0;
@@ -18,8 +24,26 @@ let marketUnit = 0;
 let marketPrice = 0;
 let marketResult = 0;
 
+const switchNav = function () {
+  resetNav();
+  this.id === "nav--mari" && appMari.classList.add("app--active");
+  this.id === "nav--todo" && appToDo.classList.add("app--active");
+  this.classList.add("btn--active");
+};
+
+const resetNav = function () {
+  if (appMari.classList.contains("app--active")) {
+    appMari.classList.remove("app--active");
+    btnNavMari.classList.remove("btn--active");
+  }
+  if (appToDo.classList.contains("app--active")) {
+    appToDo.classList.remove("app--active");
+    btnNavToDo.classList.remove("btn--active");
+  }
+};
+
 const updateResults = (element, result) => {
-  element.textContent = +result;
+  element.textContent = +result + " g";
 };
 
 const calculateBundle = (price, quantity) => {
@@ -66,3 +90,6 @@ marketPackPrice.addEventListener("input", (e) => {
   marketResult = calculateBundle(marketPrice, marketUnit);
   updateResults(marketResults, marketResult.toFixed(1));
 });
+
+btnNavToDo.addEventListener("click", switchNav);
+btnNavMari.addEventListener("click", switchNav);
